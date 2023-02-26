87bfe546b36d7732de758766504d029ca9b8a403
sumitst05 <sumitst05@gmail.com>
2023-02-26 16:37:38 +0530

    Update

---
+++ Data-Structures/Linked-List/Doubly-Linked-List/insertion.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int x) {
+        data = x;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+int main() {
+
+    return 0;
+}
