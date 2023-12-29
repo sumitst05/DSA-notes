f7a10134c318daacabb993b1bbd4ca297be87f0f
sumitst05 <sumitst05@gmail.com>
2023-12-29 21:43:56 +0530

    Added max heap and updated min heap

--- Data-Structures/Trees/Heaps/maxHeap.cpp
+++ Data-Structures/Trees/Heaps/maxHeap.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class MinHeap {
+class MaxHeap {
 public:
+    MaxHeap() {}
+
     int length() {
         return this->data.size();
     }
@@ -26,11 +28,12 @@ public:
 
         swap(this->data[0], this->data.back());
         this->data.pop_back();
-
+        
         this->heapifyDown(0);
 
         return out;
     }
+
 
 private:
     vector<int> data;
@@ -48,7 +51,7 @@ private:
     }
 
     void heapifyUp(int idx) {
-        if(this->length() == 0) {
+        if(this->data.size() == 0) {
             return;
         }
 
@@ -57,7 +60,7 @@ private:
         int pVal = this->data[pIdx];
         int val = this->data[idx];
 
-        if(val < pVal) {
+        if(val > pVal) {
             swap(this->data[idx], this->data[pIdx]);
             this->heapifyUp(pIdx);
         }
@@ -75,12 +78,12 @@ private:
             return;
         }
 
-        int minimum = min({ this->data[lIdx], this->data[idx], this->data[rIdx] });
+        int maximum = max({ lVal, val, rVal });
 
-        if(lVal == minimum) {
+        if(lVal == maximum) {
             swap(this->data[idx], this->data[lIdx]);
             this->heapifyDown(lIdx);
-        } else if(rVal == minimum) {
+        } else if(rVal == maximum) {
             swap(this->data[idx], this->data[rIdx]);
             this->heapifyDown(rIdx);
         }
@@ -88,26 +91,26 @@ private:
 };
 
 int main() {
-    MinHeap minHeap;
+    MaxHeap maxHeap;
 
-    minHeap.insert(9);
-    minHeap.insert(7);
-    minHeap.insert(2);
-    minHeap.insert(4);
-    minHeap.insert(1);
-    minHeap.insert(6);
-    minHeap.insert(3);
-    minHeap.insert(5);
-    minHeap.insert(8);
+    maxHeap.insert(9);
+    maxHeap.insert(7);
+    maxHeap.insert(2);
+    maxHeap.insert(4);
+    maxHeap.insert(1);
+    maxHeap.insert(6);
+    maxHeap.insert(3);
+    maxHeap.insert(5);
+    maxHeap.insert(8);
 
-    cout << minHeap.length() << '\n';
+    cout << maxHeap.length() << '\n';
 
-    while(!minHeap.empty()) {
-        cout << minHeap.remove() << " ";
+    while(!maxHeap.empty()) {
+        cout << maxHeap.remove() << " ";
     }
     cout << '\n';
 
-    cout << minHeap.length() << '\n';
+    cout << maxHeap.length() << '\n';
 
     return 0;
 }
